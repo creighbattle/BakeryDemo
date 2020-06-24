77ed87a5dce0b899055217c28c4995a452b86ca3
Creigh Battle <creighbattle@gmail.com>
2020-06-24 00:12:24 -0400

    Updates

---
+++ precache-manifest.c186f4b90d2e99ef7c1f6d7af3ef83ca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfac40043da1116d3a17dd46b02c6cac",
+    "url": "/BakeryDemo/index.html"
+  },
+  {
+    "revision": "fb62832d869f38382e28",
+    "url": "/BakeryDemo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "732dbfb1c0e8c8149c3c",
+    "url": "/BakeryDemo/static/css/main.66cf4aa7.chunk.css"
+  },
+  {
+    "revision": "fb62832d869f38382e28",
+    "url": "/BakeryDemo/static/js/2.63d7ac67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BakeryDemo/static/js/2.63d7ac67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732dbfb1c0e8c8149c3c",
+    "url": "/BakeryDemo/static/js/main.8a186878.chunk.js"
+  },
+  {
+    "revision": "2c8239f6dbbe0e184c80",
+    "url": "/BakeryDemo/static/js/runtime-main.2e560a16.js"
+  },
+  {
+    "revision": "baa1a10ff21baddad5363f80da9b5cbd",
+    "url": "/BakeryDemo/static/media/about-img.baa1a10f.jpg"
+  },
+  {
+    "revision": "e75c964e21a89622a7345989b824c549",
+    "url": "/BakeryDemo/static/media/chef-1.e75c964e.png"
+  },
+  {
+    "revision": "b1103f440a0d6235cfd8d0b5fe9052c8",
+    "url": "/BakeryDemo/static/media/chef-2.b1103f44.png"
+  },
+  {
+    "revision": "f72b44bbd1339b3b3ffd2df785d52eb5",
+    "url": "/BakeryDemo/static/media/chef-3.f72b44bb.png"
+  },
+  {
+    "revision": "ed9fd5a5236a072aebe24074fd114e2d",
+    "url": "/BakeryDemo/static/media/header-bg.ed9fd5a5.jpg"
+  },
+  {
+    "revision": "40aa99d607251118d3e25e40d9351124",
+    "url": "/BakeryDemo/static/media/logo1.40aa99d6.png"
+  },
+  {
+    "revision": "a613d1c43f43b26559f65d072da9099d",
+    "url": "/BakeryDemo/static/media/menu-item-1.a613d1c4.jpg"
+  },
+  {
+    "revision": "389c0e09b861f922602bb8b4492eb17e",
+    "url": "/BakeryDemo/static/media/menu-item-2.389c0e09.jpg"
+  },
+  {
+    "revision": "b2f5b072298bd1b0a9f32bff53689289",
+    "url": "/BakeryDemo/static/media/menu-item-3.b2f5b072.jpg"
+  },
+  {
+    "revision": "1d499f59d189cd5e253a1e1e99c189b0",
+    "url": "/BakeryDemo/static/media/menu-item-4.1d499f59.jpg"
+  },
+  {
+    "revision": "26cec5bcd1400b48f3f518b97f5ae893",
+    "url": "/BakeryDemo/static/media/menu-item-5.26cec5bc.jpg"
+  },
+  {
+    "revision": "2e1004a416300e34d4975cf4c4ce9b38",
+    "url": "/BakeryDemo/static/media/menu-item-6.2e1004a4.jpg"
+  }
+]);
